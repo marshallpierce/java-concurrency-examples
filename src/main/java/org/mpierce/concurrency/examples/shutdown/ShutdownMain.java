ece543766cddeffd6eb90c7cbfdd4469dd0f1b2a
Marshall Pierce <marshall@mpierce.org>
2015-03-22 16:18:18 -0700

    Improve ShutdownMain to show cancling both raw threads and futures

--- src/main/java/org/mpierce/concurrency/examples/shutdown/ShutdownMain.java
+++ src/main/java/org/mpierce/concurrency/examples/shutdown/ShutdownMain.java
@@ -2,20 +2,31 @@ package org.mpierce.concurrency.examples.shutdown;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 public class ShutdownMain {
-    public static void main(String[] args) {
-//        rawThread();
+    public static void main(String[] args) throws InterruptedException {
+        rawThread();
         executor();
     }
 
-    private static void executor() {
+    private static void executor() throws InterruptedException {
         ExecutorService executorService = Executors.newCachedThreadPool();
-        executorService.submit(new Sleeper());
+        Future<?> future = executorService.submit(new Sleeper());
+
+        Thread.sleep(50);
+
+        future.cancel(true);
+        executorService.shutdown();
     }
 
-    private static void rawThread() {
+    private static void rawThread() throws InterruptedException {
         Thread thread = new Thread(new Sleeper());
+        thread.start();
+
+        Thread.sleep(50);
+
+        thread.interrupt();
     }
 
     private static class Sleeper implements Runnable {
@@ -25,6 +36,7 @@ public class ShutdownMain {
                 try {
                     Thread.sleep(100);
                 } catch (InterruptedException e) {
+                    System.out.println("Exiting loop in " + Thread.currentThread().getName());
                     Thread.currentThread().interrupt();
                     return;
                 }
