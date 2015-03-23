7088d1c009a5e89e5806e4e497a17c483488f4a9
Marshall Pierce <marshall@mpierce.org>
2015-03-22 20:14:34 -0700

    Add another layer of try/finally

--- src/main/java/org/mpierce/concurrency/examples/diningphilosophers/waiter/WaiterPhilosopher.java
+++ src/main/java/org/mpierce/concurrency/examples/diningphilosophers/waiter/WaiterPhilosopher.java
@@ -1,39 +1,39 @@
 package org.mpierce.concurrency.examples.diningphilosophers.waiter;
 
-import org.mpierce.concurrency.examples.diningphilosophers.AbstractPhilosopher;
-import javax.annotation.Nonnull;
-
 import java.util.concurrent.CountDownLatch;
+import javax.annotation.Nonnull;
+import org.mpierce.concurrency.examples.diningphilosophers.AbstractPhilosopher;
 
 final class WaiterPhilosopher extends AbstractPhilosopher<WaiterChopstick> {
 
     private final Waiter waiter;
 
     WaiterPhilosopher(@Nonnull CountDownLatch latch, @Nonnull WaiterChopstick left, @Nonnull WaiterChopstick right,
-                      int numEats,
-                      Waiter waiter) {
+        int numEats,
+        Waiter waiter) {
         super(latch, left, right, numEats);
         this.waiter = waiter;
     }
 
-
     protected void eat(int mealsEaten) throws InterruptedException {
         this.waiter.acquirePair(this);
 
-        this.left.acquireOwnership();
         try {
-            this.right.acquireOwnership();
+            this.left.acquireOwnership();
             try {
-                logger.info("eating meal #" + mealsEaten);
-                randomSleep();
+                this.right.acquireOwnership();
+                try {
+                    logger.info("eating meal #" + mealsEaten);
+                    randomSleep();
+                } finally {
+                    this.right.releaseOwnership();
+                }
             } finally {
-                this.right.releaseOwnership();
+                this.left.releaseOwnership();
             }
         } finally {
-            this.left.releaseOwnership();
+            this.waiter.releasePair(this);
         }
 
-        this.waiter.releasePair(this);
     }
-
 }
