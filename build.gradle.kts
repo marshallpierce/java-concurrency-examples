e2f89eba6b66a4ce74374a71b7ac28eec2db1215
Marshall Pierce <marshall@mpierce.org>
2019-07-18 15:01:26 +0300

    Get it building on modern JVMs

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+plugins {
+  java
+  id("me.champeau.gradle.jmh") version "0.5.0-rc-1"
+}
+
+val deps by extra {
+  mapOf(
+          "jmh" to "1.21",
+          "slf4j" to "1.7.26"
+  )
+}
+
+repositories {
+  jcenter()
+}
+
+java {
+  sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+  implementation("junit:junit:4.12")
+  implementation("org.slf4j:slf4j-api:${deps["slf4j"]}")
+  implementation("org.slf4j:slf4j-ext:${deps["slf4j"]}")
+  implementation("org.jboss.netty:netty:3.2.10.Final")
+  implementation("commons-codec:commons-codec:1.4")
+  implementation("com.google.guava:guava:18.0")
+  implementation("joda-time:joda-time:2.7")
+  implementation("com.google.code.findbugs:jsr305:3.0.0")
+
+  runtime("ch.qos.logback:logback-classic:0.9.26")
+
+  /* help intellij figure out jmh*/
+  implementation("org.openjdk.jmh:jmh-core:${deps["jmh"]}")
+}
+
+jmh {
+  jmhVersion = deps["jmh"]
+  include = listOf(".*Benchmark")
+}
+
